340502557e55f5a0d443d76494d0de8ee2dd125b
shiodev <ndnghi.2000@gmail.com>
2023-08-02 17:52:41 +0700

    change logo

--- resources/views/partials/admin/navbar.blade.php
+++ resources/views/partials/admin/navbar.blade.php
@@ -1,8 +1,12 @@
 <nav class="navbar col-lg-12 col-12 p-0 fixed-top d-flex flex-row">
 
   <div class="text-center navbar-brand-wrapper d-flex align-items-center justify-content-center">
-    <a class="navbar-brand brand-logo mr-5" href="{{ route('admin.dashboard') }}"><img src="{{ asset('backend/images/logo.svg') }}" class="mr-2" alt="logo" /></a>
-    <a class="navbar-brand brand-logo-mini" href="{{ route('admin.dashboard') }}"><img src="{{ asset('backend/images/logo-mini.svg') }}" alt="logo" /></a>
+    <a class="navbar-brand brand-logo mr-5" href="{{ route('admin.dashboard') }}">
+      <img src="{{ asset('images/logo.png') }}" alt="shiobook" width="150px" class="mr-2" style="height: 25px !important;">
+    </a>
+    <a class="navbar-brand brand-logo-mini" href="{{ route('admin.dashboard') }}">
+      <img src="{{ asset('images/logo-mini.png') }}" alt="shiobook" width="120px" class="mr-2" style="height: 25px !important;">
+    </a>
   </div>
 
   <div class="navbar-menu-wrapper d-flex align-items-center justify-content-end">
