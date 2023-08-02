40e5f778f404170277039bc64f6e81df99250ed1
shiodev <ndnghi.2000@gmail.com>
2023-08-02 21:19:29 +0700

    ico

--- resources/views/partials/layouts/admin.blade.php
+++ resources/views/partials/layouts/admin.blade.php
@@ -5,7 +5,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <link rel="icon" href="{{ asset('backend/images/logo-mini.svg') }}">
+  <link rel="icon" href="{{ asset('images/logo.ico') }}">
   <link rel="stylesheet" href="{{ asset('backend/vendors/feather/feather.css') }}">
   <link rel="stylesheet" href="{{ asset('backend/vendors/ti-icons/css/themify-icons.css') }}">
   <link rel="stylesheet" href="{{ asset('backend/vendors/font-awesome/css/all.min.css') }}">
--- resources/views/partials/layouts/client.blade.php
+++ resources/views/partials/layouts/client.blade.php
@@ -5,6 +5,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <link rel="stylesheet" href="{{ asset('frontend/css/app.css') }}">
+  <link rel="icon" href="{{ asset('images/logo.ico') }}">
   <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.min.css" />
   @stack('css')
